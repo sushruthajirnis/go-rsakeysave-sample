e0a1d64523f61d0e08d779c049ec8d85e0e23699
Hajirnis, Sushrut <sushrut.hajirnis@gmail.com>
2022-06-09 15:31:43 -0700

    go rsa private key save and read from file sample

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func main() {
+	priv, _ := GenerateRsaKeyPair()
+	privBytes := []byte(ExportRsaPrivateKeyAsPemStr(priv))
+	err := os.WriteFile("priv.pem", privBytes, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	dat, err := os.ReadFile("priv.pem")
+
+	privParsed, err := ParseRsaPrivateKeyFromPemStr(string(dat))
+
+	if !priv.Equal(privParsed) {
+		fmt.Println("Writing was a problem then")
+	}
+
+}
+
+func GenerateRsaKeyPair() (*rsa.PrivateKey, *rsa.PublicKey) {
+	privkey, _ := rsa.GenerateKey(rand.Reader, 4096)
+	return privkey, &privkey.PublicKey
+}
+func ExportRsaPrivateKeyAsPemStr(privkey *rsa.PrivateKey) string {
+	privkeyBytes := x509.MarshalPKCS1PrivateKey(privkey)
+	privkeyPem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privkeyBytes,
+		},
+	)
+	return string(privkeyPem)
+}
+
+func ParseRsaPrivateKeyFromPemStr(privPEM string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privPEM))
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
